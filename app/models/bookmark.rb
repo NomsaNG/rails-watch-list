6132183037ff755c548b34fb16c0bfe01ee161e4
Nomsa Goba <nomsanatashagoba@gmail.com>
2023-11-16 12:23:36 +0200

    second commit, models completed and seeds.

---
+++ app/models/bookmark.rb
@@ -0,0 +1,8 @@
+class Bookmark < ApplicationRecord
+  belongs_to :movie
+  belongs_to :list
+  # validates_associated :movie, :list
+  validates :comment, length: { minimum: 6 }
+  validates :movie_id, presence: true
+  validates_uniqueness_of :movie_id, scope: :list_id
+end
